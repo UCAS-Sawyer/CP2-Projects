b80edd8e567191f26b4a000fa6cf737ce6c2d3eb
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2025-01-09 13:44:28 -0700

    created functions

--- financial_calculator/main.py
+++ financial_calculator/main.py
@@ -10,23 +10,78 @@
 #Tip Calculator
 
 #---
+while True:
 
-def main():
-    pass
 
-def saveingcalculator():
+    def saveingcalculator():
 
-    #Inputs for the needed variables
-    Income = input("What is your weekly income.")
-    MoneySavedGoal = input("How much money do you want to save?\t")
-    WeeklyDepositAmount =  input("How much are you going to deposit each week.")
+        #Inputs for the needed variables
+        Income = int(input("\nWhat is your weekly income.\t"))
+        MoneySavedGoal = int(input("\nHow much money do you want to save?\t"))
+        WeeklyDepositAmount =  int(input("\nHow much are you going to deposit each week.\t"))
 
-    #Making sure the amount is valid
-    if WeeklyDepositAmount > Income:
-        print("You can not deposit that amount per week")
-    elif WeeklyDepositAmount <=0:
-        print("You aren't depositing anything.")
-    
-    print("It will take", MoneySavedGoal/WeeklyDepositAmount)
+        #Making sure the amount is valid
+        if WeeklyDepositAmount > Income:
+            print("\nYou can not deposit that amount per week")
+        elif WeeklyDepositAmount <=0:
+            print("\nYou aren't depositing anything.")
+        
+        print("\nIt will take", MoneySavedGoal/WeeklyDepositAmount, "weeks.")
 
-Desiction = input("What do you want to do?(1:Calculate savings, 2:Coumpound Interest Calculator, 3:Budget Allocator, 4:Sale Prive Calculator, 5:Tip Calculator)")
+    def InterestCalc():
+        #Inputs for the needed variables
+        AmountOMoney = int(input("\nWhat is the initial amount of money in the acount?\t"))
+        InterestAmountPerYear = int(input("\nWhat is the interest you get per year(Percentage)?\t"))
+        AmountAddedEachYear = int(input("\nHow much are you going to add per year?\t"))
+        AmountInYears = int(input("\nHow many years do you want to calculate\t"))
+
+        InterestAmountPerYear = InterestAmountPerYear/100
+
+        #exception handling
+        if InterestAmountPerYear <= 0:
+            print("You can't have negative interest.")
+        
+        if AmountOMoney < 0:
+            print("This calculator does not want a negative in the starting amount.")
+
+        #Calculating Math
+        for x in range(AmountInYears):
+            AmountOMoney = ((AmountOMoney + AmountAddedEachYear)*(1 + InterestAmountPerYear))
+        
+        print(f"After {AmountInYears} years there would be {AmountOMoney} in the acount.")
+            
+    def BudgetAllocator():
+        print("dsafgdsahfgsafdsafdrkugdsafdgeraesdlufrva")
+
+    def SalePriceCalc():
+        print("dsafsa")
+
+    def TipCalc():
+        #Needed variables
+        CostOFood = int(input(""))
+
+    def main():
+
+        Decision = input("\nWhat do you want to do?(1:Calculate savings, 2:Coumpound Interest Calculator, 3:Budget Allocator, 4:Sale Prive Calculator, 5:Tip Calculator, p:Exit)\t")
+
+        #Checking all options for Decision
+        if Decision == "1":
+            saveingcalculator()
+
+        elif Decision == "2":
+            InterestCalc()
+        
+        elif Decision == "3":
+            BudgetAllocator()
+
+        elif Decision == "4":
+            SalePriceCalc()
+
+        elif Decision == "5":
+            TipCalc()
+        
+        elif Decision == "p":
+            #Stopping the program
+            raise SystemExit
+
+    main()
