6a7dbb8c85cba609cf0367aac59d2602e25ba40b
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2025-01-23 19:09:45 -0700

    Finished up the actual making of the passwords

--- RandomPasswordGenrator/main.py
+++ RandomPasswordGenrator/main.py
@@ -2,13 +2,12 @@
 
 #Importing random library and string library
 import random
-import string
 
 #Required lists for creating the password
 LowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 UppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 Numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-Symbols = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
+Symbols = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~']
 
 #Exception handling for ints
 def intchecker(Inputx):
