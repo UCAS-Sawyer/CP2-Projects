09cb6184f628ebc96b77729f41c95cd07bcf9931
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2025-02-05 13:26:04 -0700

    Fixed variable names to follow camel case.

--- PersonalLibraryProgram/main.py
+++ PersonalLibraryProgram/main.py
@@ -1,70 +1,70 @@
-#Sawyer Wood, Personal Library Program
+#Sawyer Wood, Personal library Program
 
 #Needs a set or a tuple somewhere.
 
-Library = []
+library = []
 
-def intchecker(Inputx):
+def intchecker(inputx):
     try:
-        Inputx = int(Inputx)
-        return(Inputx)
+        inputx = int(inputx)
+        return(inputx)
     except:
         print("\nThat is not a valid input.")
         return None
 
-def AddItem(Library):
+def AddItem(library):
     #Necassary inputs  and invalid checking
-    NameOfBook = input("\nWhat is the name of the book?\t")
-    NameOfAuthor = input("\nWhat is the name of the author?\t")
-    NumberOfPages = intchecker(input("\nHow many pages does it have?\t"))
-    if NumberOfPages != None:
-        YearPublished = intchecker(input("\nWhat year was the book published?\t"))
-        if YearPublished != None:
-            Item = {
-                "Name" : NameOfBook, 
-                "Author" : NameOfAuthor, 
-                "#Pages" : NumberOfPages,
-                "Year" : YearPublished
+    nameOfBook = input("\nWhat is the name of the book?\t")
+    nameOfAuthor = input("\nWhat is the name of the author?\t")
+    numberOfPages = intchecker(input("\nHow many pages does it have?\t"))
+    if numberOfPages != None:
+        yearPublished = intchecker(input("\nWhat year was the book published?\t"))
+        if yearPublished != None:
+            item = {
+                "Name" : nameOfBook, 
+                "Author" : nameOfAuthor, 
+                "#Pages" : numberOfPages,
+                "Year" : yearPublished
                 }
 
             print(f"\nThe book has been added.")
 
             #Adding the item to the list
-            Library.append(Item)
+            library.append(item)
 
-def RemoveItem(Library):
+def RemoveItem(library):
     #Necassary inputs and checking if it is in the library
-    NameOfBook = input("\nWhat is the name of the book you would like to remove?\t")
-    for item in Library:
+    nameOfBook = input("\nWhat is the name of the book you would like to remove?\t")
+    for item in library:
         #Checking if the title of the book is in the library and removing it"
-        if NameOfBook == item["Name"]:
+        if nameOfBook == item["Name"]:
             print("\n", item["Name"], "has been removed.")
-            Library.remove(item)
+            library.remove(item)
         else:
             print("\nThat is not one of the titles of any of the books in the library.\t")
 
-def SearchForItem(Library):
+def SearchForItem(library):
     found = False
     #Necassary inputs and valid checking
-    AuthorOrTitle = input("\nDo you want to seach by 1: Author Name or 2: Book Title ?\t")
+    authorOrTitle = input("\nDo you want to seach by 1: Author Name or 2: Book Title ?\t")
     
-    if AuthorOrTitle == "1":
+    if authorOrTitle == "1":
         #Necassary input
-        AuthorName = input("\nWhat is the author's name?\t")
-        for item in Library:
+        authorname = input("\nWhat is the author's name?\t")
+        for item in library:
             #Checking if any of the books are by the author
-            if AuthorName == item["Author"]:
+            if authorname == item["Author"]:
                 print(f"\nTitle: {item["Name"]}, Author's Name: {item["Author"]}, Number of Pages: {item["#Pages"]}, Year Published: {item["Year"]}")
                 found = True
         if found == False:
             print("\nThere are no books by that author in here.")
 
-    elif AuthorOrTitle == "2":
+    elif authorOrTitle == "2":
         #Necassary input
-        BookTitle = input("\nWhat is the title of the book?\t")
-        for item in Library:
+        bookTitle = input("\nWhat is the title of the book?\t")
+        for item in library:
             #Checking if any of the books have the title
-            if BookTitle == item["Name"]:
+            if bookTitle == item["Name"]:
                 print(f"\nTitle: {item["Name"]}, Author's Name: {item["Author"]}, Number of Pages: {item["#Pages"]}, Year Published: {item["Year"]}")
                 found = True
         if found == False:
@@ -72,19 +72,19 @@ def SearchForItem(Library):
     else:
         print("\nThat is not one of the options.")
 
-def PrintingLbrary(Library):
+def PrintingLbrary(library):
     printingStuff = ""
     printingAuthors = set()
     disicion = input("\nDo you want to 1: Display Everything, 2: Display Only Book Names, 3: Display Only Authors? \t")
 
     if disicion == "1":
-        print("\n", Library)
+        print("\n", library)
     elif disicion == "2":
-        for book in Library:
+        for book in library:
             printingStuff += book["Name"]
         print([printingStuff])
     elif disicion == "3":
-        for book in Library:
+        for book in library:
             printingAuthors.add(book["Author"])
         print(printingAuthors)
     else:
@@ -92,24 +92,24 @@ def PrintingLbrary(Library):
     
 
 def main():
-    Disicion = input("\nDo you want to 1: Add an item, 2: Remove an item, 3: Search for an item, 4: Display Library, 5: Exit ? \t")
+    disicion = input("\nDo you want to 1: Add an item, 2: Remove an item, 3: Search for an item, 4: Display library, 5: Exit ? \t")
 
-    if Disicion == "1":
-        AddItem(Library)
-    elif Disicion == "2":
-        RemoveItem(Library)
-    elif Disicion == "3":
-        SearchForItem(Library)
-    elif Disicion == "4":
-        PrintingLbrary(Library)
-    elif Disicion == "5":
+    if disicion == "1":
+        AddItem(library)
+    elif disicion == "2":
+        RemoveItem(library)
+    elif disicion == "3":
+        SearchForItem(library)
+    elif disicion == "4":
+        PrintingLbrary(library)
+    elif disicion == "5":
         raise SystemExit
     else:
         print("\nThat is not one of the options.")
 
 #Clearing Screen
 print("\033[H\033[J")
-print("\nWelcome to the Personal Library Program(PLP)")
+print("\nWelcome to the Personal library Program(PLP)")
 
 while True:
     main()
