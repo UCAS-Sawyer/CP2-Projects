5212e91ea6e4b7bd345436b6a3b0f01087f2261a
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2025-02-11 13:34:32 -0700

    Added printing all titles

--- MovieRecommender/main.py
+++ MovieRecommender/main.py
@@ -9,13 +9,28 @@ with open("MovieRecommender/Movies list.csv") as file:
     csvReader = csv.reader(file)
     next(csvReader)
     for row in csvReader:
-        item = {}
-        item.update({"Title" : row[0]})
-        item.update({"Director" : row[1]})
-        item.update({"Genre" : row[2]})
-        item.update({"Rating" : row[3]})
-        item.update({"Length (min)" : row[4]})
-        item.update({"Notable Actors" : row[5]})
+        item = {
+            "Title" : row[0],
+            "Director" : row[1],
+            "Genre" : row[2],
+            "Rating" : row[3],
+            "Length (min)" : row[4],
+            "Notable Actors" : row[5]
+        }
+
         movies.append(item)
 
-print(movies)
+def PrintMovieTitles(movies):
+    movieTitles = []
+    for item in movies:
+        movieTitles.append(item["Title"])
+
+    print(movieTitles)
+
+
+def main():
+    choice = input("\nWhat would you like to do? 1: See All Movie Titles, 2: Search, 3: See All Information, 4: Exit\n")
+    if choice == "1":
+        PrintMovieTitles(movies)
+
+main()
