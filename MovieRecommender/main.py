4aa4637b9332a4c065e8e44def0c5040e0542026
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2025-02-13 13:07:35 -0700

    made the list of moveis lowercase

--- MovieRecommender/main.py
+++ MovieRecommender/main.py
@@ -10,12 +10,12 @@ with open("MovieRecommender/Movies list.csv") as file:
     next(csvReader)
     for row in csvReader:
         item = {
-            "Title" : row[0],
-            "Director" : row[1],
-            "Genre" : row[2],
-            "Rating" : row[3],
-            "Length (min)" : row[4],
-            "Notable Actors" : row[5]
+            "Title" : row[0].lower(),
+            "Director" : row[1].lower(),
+            "Genre" : row[2].lower(),
+            "Rating" : row[3].lower(),
+            "Length (min)" : row[4].lower(),
+            "Notable Actors" : row[5].lower()
         }
 
         movies.append(item)
@@ -32,6 +32,8 @@ def main():
     choice = input("\nWhat would you like to do? 1: See All Movie Titles, 2: Search, 3: See All Information, 4: Exit\n")
     if choice == "1":
         PrintMovieTitles(movies)
+    elif choice == "3":
+        print(movies)
     elif choice == "4":
         raise SystemExit
 
