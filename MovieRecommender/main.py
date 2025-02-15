b435fb98e06fb2e260898149b2a5c2350c7eed75
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2025-02-15 12:28:21 -0700

    created filters_function

--- MovieRecommender/main.py
+++ MovieRecommender/main.py
@@ -4,11 +4,11 @@ import csv
 
 movies = []
 
-#Creating a list of dictionaries of each item
+#Creating a dictionary for each item and then storing it in a list
 with open("MovieRecommender/Movies list.csv") as file:
-    csvReader = csv.reader(file)
-    next(csvReader)
-    for row in csvReader:
+    csv_reader = csv.reader(file)
+    next(csv_reader)
+    for row in csv_reader:
         item = {
             "Title" : row[0].lower(),
             "Director" : row[1].lower(),
@@ -20,23 +20,68 @@ with open("MovieRecommender/Movies list.csv") as file:
 
         movies.append(item)
 
-def PrintMovieTitles(movies):
-    movieTitles = []
+#Exception handling for ints
+def intchecker(inputx):
+    try:
+        #Turning it into the int and returning it
+        inputx = int(inputx)
+        return inputx
+    except:
+        #If it can't be turned into a int then it will return them back to the UI 
+        print("\nThat is an invalid input.")
+        return None
+
+def print_movie_titles(movies):
+    movie_titles = []
     for item in movies:
-        movieTitles.append(item["Title"])
+        movie_titles.append(item["Title"])
 
-    print(movieTitles)
+    print(movie_titles)
 
-def Search(movies):
-    print("\nWorkInProgress!")
-    print("Vincent was here.")
+def filters_function():
+    filters = []
+
+    while True:
+        filter_pair = []
+        filter_choice = input("\nWhat do you want you first filter to be, 1: Genre, 2: Directors, 3: Length, 4: Actors, 5: Move on\n")
+
+        if filter_choice == "1":
+            filter = "Genre"
+        elif filter_choice == "2":
+            filter = "Director"
+        elif filter_choice == "3":
+            filter = "Length (min)"
+        elif filter_choice == "4":
+            filter = "Notable Actors"  
+        elif filter_choice == "5":
+            break
+        else:
+            print("\nThat is not an option.")
+
+        filter_data = input(f"\nWhat {filter} do you want to be recomended?\n")
+        filter_data = filter_data.strip()
+
+        if filter == "Lenght (min)":
+            intchecker = intchecker(filter_data)
+            if intchecker == None:
+                continue
+        
+        filter_pair.append(filter)
+        filter_pair.append(filter_data)
+        filters.append(filter_pair)
+
+    return filters
+
+def search(movies):
+    filters = filters_function()
+
 
 def main():
-    choice = input("\nWhat would you like to do? 1: See All Movie Titles, 2: Search, 3: See All Information, 4: Exit\n")
+    choice = input("\nWhat would you like to do? 1: See All Movie Titles, 2: Get Recomendations, 3: See All Information, 4: Exit\n")
     if choice == "1":
-        PrintMovieTitles(movies)
+        print_movie_titles(movies)
     elif choice == "2":
-        Search(movies)
+        search(movies)
     elif choice == "3":
         print(movies)
     elif choice == "4":
