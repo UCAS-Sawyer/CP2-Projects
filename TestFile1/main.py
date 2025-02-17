72994cb7fce9936a489bb3dab8d0f9d9038e1f86
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2025-02-17 16:22:37 -0700

    Example of using lambda

---
+++ TestFile1/main.py
@@ -0,0 +1,15 @@
+students = [
+    {"Name" : 'Alice', 
+     "Letter" : 'B', 
+     "Age": 18},
+     {"Name" : 'Alice', 
+     "Letter" : 'B', 
+     "Age": 17},
+     {"Name" : 'Alice', 
+     "Letter" : 'B', 
+     "Age": 19}
+]
+
+# Sort by age (third element of each tuple)
+sorted_by_age = sorted(students, key=lambda student: student["Age"])
+print(sorted_by_age)
