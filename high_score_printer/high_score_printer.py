7f1d83bb08b270b9608699fba3ef378382ce098d
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2025-02-25 13:41:50 -0700

    Cool fontsandcolor

--- high_score_printer/high_score_printer.py
+++ high_score_printer/high_score_printer.py
@@ -1,9 +1,11 @@
 #Sawyer Wood, Functions to print the top five highscores
 
 import csv
+import fontstyle
 
 file_path = "high_score_printer/highscores.csv"
 
+#It requires the file path to be sent to it
 def high_score_printer_complexgame(file_path):
 
     #Clearing Screen
@@ -28,11 +30,20 @@ def high_score_printer_complexgame(file_path):
         sorted_by_score = sorted(highscores, key = lambda gamer: gamer["highscore"], reverse = True)
 
         #Printing all the scores with the names and ranks
-        print("Rank    Name     Score")
+        print("Rank    Name    Score")
         for x in sorted_by_score:
             player_number += 1
             if player_number <= 5:
-                print(f"{player_number}\t{x["name"]}\t {x["highscore"]}")
+                if player_number == 1:
+                    print(fontstyle.apply(f"{player_number}\t{x["name"]}\t{x["highscore"]}", "underline/bold/Italic/yellow"))
+                elif player_number == 2:
+                    print(fontstyle.apply(f"{player_number}\t{x["name"]}\t{x["highscore"]}", "bold/Italic/green"))
+                elif player_number == 3:
+                    print(fontstyle.apply(f"{player_number}\t{x["name"]}\t{x["highscore"]}", "Italic/cyan"))
+                elif player_number == 4:
+                    print(fontstyle.apply(f"{player_number}\t{x["name"]}\t{x["highscore"]}", "purple"))
+                elif player_number == 5:
+                    print(fontstyle.apply(f"{player_number}\t{x["name"]}\t{x["highscore"]}", "red"))
             else:
                 break
     #Back to where you came from
