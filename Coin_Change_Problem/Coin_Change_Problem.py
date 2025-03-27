e92875d7a2b6e073d85ad5c8381d5d031c79a918
UCAS-Sawyer <sawyer.wood@cas-edu.net>
2025-03-27 13:35:48 -0600

    inner functions yah

--- Coin_Change_Problem/Coin_Change_Problem.py
+++ Coin_Change_Problem/Coin_Change_Problem.py
@@ -4,48 +4,46 @@ import csv
 
 def coin_change_main():
     currency = currency_type()
-    coins_and_bills = reading_csv(currency)
+    coins_and_bills = reading_csv()
     print(coins_and_bills)
-
-
-def currency_type():
-    currencies = "1. US Dollar, 2. Euro, 3. Pound, 4. Japanese Yen"
-    currency = None
-
-    currecny_type_choice = input(f"\nWhich available currecny are you inputing? {currencies}\n")
-
-    if currecny_type_choice == "1":
-        currency = "USD"
-    elif currecny_type_choice == "2":
-        currency = "Euro"
-    elif currecny_type_choice == "3":
-        currency = "Pound"
-    elif currecny_type_choice == "4":
-        currency = "JYen"
-    else:
-        print("\nThat is not a valid input.")
-        currency_type()
-        return currency
     
-    print(f"You have selected the currecy {currency}.")
-    return currency
+    def currency_type():
+        currencies = "1. US Dollar, 2. Euro, 3. Pound, 4. Japanese Yen"
+        currency = None
 
+        currecny_type_choice = input(f"\nWhich available currecny are you inputing? {currencies}\n")
 
-def reading_csv(currency_type):
-    #List of all the coins and bills
-    coins_and_bills = []
+        if currecny_type_choice == "1":
+            currency = "USD"
+        elif currecny_type_choice == "2":
+            currency = "Euro"
+        elif currecny_type_choice == "3":
+            currency = "Pound"
+        elif currecny_type_choice == "4":
+            currency = "JYen"
+        else:
+            print("\nThat is not a valid input.")
+            currency_type()
+            return currency
+        
+        print(f"You have selected the currecy {currency}.")
+        return currency
 
-    with open(f"Coin_Change_Problem\\csvs\\{currency_type}.csv", "r") as file:
-        csv_reader = csv.reader(file)
-        next(csv_reader)
+    def reading_csv():
+        #List of all the coins and bills
+        coins_and_bills = []
 
-        for row in csv_reader:
-            #Creating the dictionary
+        with open(f"Coin_Change_Problem\\csvs\\{currency}.csv", "r") as file:
+            csv_reader = csv.reader(file)
+            next(csv_reader)
 
-            coin_or_bill = {
-                "name" : row[0],
-                "amount" : int(row[1]),
-            }
-            coins_and_bills.append(coin_or_bill)
+            for row in csv_reader:
+                #Creating the dictionary
 
-        return coins_and_bills
+                coin_or_bill = {
+                    "name" : row[0],
+                    "amount" : int(row[1]),
+                }
+                coins_and_bills.append(coin_or_bill)
+
+            return coins_and_bills
