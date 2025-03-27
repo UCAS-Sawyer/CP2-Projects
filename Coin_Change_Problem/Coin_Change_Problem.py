b794685b3bc2a375c900dc8dac4fba7e1f6452fa
UCAS-Sawyer <sawyer.wood@cas-edu.net>
2025-03-27 13:32:34 -0600

    you can now open the csvs

--- Coin_Change_Problem/Coin_Change_Problem.py
+++ Coin_Change_Problem/Coin_Change_Problem.py
@@ -1,7 +1,12 @@
 #Sawyer Wood Coin Changer problem
 
+import csv
+
 def coin_change_main():
     currency = currency_type()
+    coins_and_bills = reading_csv(currency)
+    print(coins_and_bills)
+
 
 def currency_type():
     currencies = "1. US Dollar, 2. Euro, 3. Pound, 4. Japanese Yen"
@@ -11,12 +16,12 @@ def currency_type():
 
     if currecny_type_choice == "1":
         currency = "USD"
-    if currecny_type_choice == "1":
-        currency = "USD"
-    if currecny_type_choice == "1":
-        currency = "USD"
-    if currecny_type_choice == "1":
-        currency = "USD"
+    elif currecny_type_choice == "2":
+        currency = "Euro"
+    elif currecny_type_choice == "3":
+        currency = "Pound"
+    elif currecny_type_choice == "4":
+        currency = "JYen"
     else:
         print("\nThat is not a valid input.")
         currency_type()
@@ -25,3 +30,22 @@ def currency_type():
     print(f"You have selected the currecy {currency}.")
     return currency
 
+
+def reading_csv(currency_type):
+    #List of all the coins and bills
+    coins_and_bills = []
+
+    with open(f"Coin_Change_Problem\\csvs\\{currency_type}.csv", "r") as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)
+
+        for row in csv_reader:
+            #Creating the dictionary
+
+            coin_or_bill = {
+                "name" : row[0],
+                "amount" : int(row[1]),
+            }
+            coins_and_bills.append(coin_or_bill)
+
+        return coins_and_bills
