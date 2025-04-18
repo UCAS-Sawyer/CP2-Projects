666c70a7113bcdb32e57f561f8e8c1edcdb6936e
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2025-04-18 13:44:06 -0600

    Everything works

--- Geometry_Calculator/ShapesNMath.py
+++ Geometry_Calculator/ShapesNMath.py
@@ -1,6 +1,6 @@
 #Geometry Calculator Math File
 
-from checkers import floatchecker
+from checkers import floatchecker, unitchecker
 
 class Rectangle:
 
@@ -10,20 +10,59 @@ class Rectangle:
     def perimeter(self):
         return 2 * (self.length + self.width)
 
-    def display_results(self):
+    def display_results(self, unit):
         area = self.area()
         perimeter = self.perimeter()
-        print(f"\nThe area of the rectangle is: {area} and the perimeter is: {perimeter}.")
+        print(f"\nThe area of the rectangle is: {area} {unit} squared and the perimeter is: {perimeter} {unit}.")
 
     def __init__(self):
-        self.length = floatchecker(input("\nWhat is the legnth of the rectangle:\n"), "length")
+        self.unit = unitchecker(input("\nWhat unit of measurement are you using?(Make sure it is like, mm, cm, in, ft, ect..)\n"))
+        self.length = floatchecker(input("\nWhat is the length of the rectangle:\n"), "length")
         self.width = floatchecker(input("\nWhat is the width of the rectangle:\n"), "width")
-        self.display_results()
+        self.display_results(self.unit)
     
+class Triangle:
 
+    def area(self):
+        return self.width * (self.height / 2)
 
-def Triangle():
-    print("YLO")
+    def perimeter(self, width):
+        self.side2 = floatchecker(input("\nWhat is the length of the second side of the triangle:\n"), "second side")
+        self.side3 = floatchecker(input("\nWhat is the length of the third side of the triangle:\n"), "third side")
+        
+        if self.width + self.side2 > self.side3 and self.width + self.side3 > self.side2 and self.side2 + self.side3 > self.width:
+            return self.width + self.side2 + self.side3
+        else:
+            print("\nThat is not a valid triangle, try again.")
+            self.perimeter(width)
+            return self.width + self.side2 + self.side3
+            
 
-def Circle():
-    print("YLO")
+    def display_results(self, width, unit):
+        area = self.area()
+        perimeter = self.perimeter(width)
+        print(f"\nThe area of the triangle is: {area} {unit} squared and the perimeter is: {perimeter} {unit}.")
+
+    def __init__(self):
+        self.unit = unitchecker(input("\nWhat unit of measurement are you using?(Make sure it is like, mm, cm, in, ft, ect..)\n"))
+        self.height = floatchecker(input("\nWhat is the height of the triangle:\n"), "length")
+        self.width = floatchecker(input("\nWhat is the width of the triangle:\n"), "width")
+        self.display_results(self.width, self.unit)
+
+class Circle:
+
+    def area(self):
+        return round(3.14159 * (self.radius ** 2), 2)
+
+    def circumference(self):
+        return round(2 * 3.14159 * self.radius, 2)
+
+    def display_results(self, unit):
+        area = self.area()
+        circumference = self.circumference()
+        print(f"\nThe area of the circle is: {area} {unit} squared and the circumference is: {circumference} {unit}.")
+
+    def __init__(self):
+        self.unit = unitchecker(input("\nWhat unit of measurement are you using?(Make sure it is like, mm, cm, in, ft, ect..)\n"))
+        self.radius = floatchecker(input("\nWhat is the radius of the circle:\n"), "radius")
+        self.display_results(self.unit)
--- Geometry_Calculator/checkers.py
+++ Geometry_Calculator/checkers.py
@@ -8,3 +8,16 @@ def floatchecker(number, TypeONumber):
         print("\nPlease enter a valid number.")
         number = floatchecker(input(f"\nRetry to enter the {TypeONumber}: "), TypeONumber)
         return number
+
+def unitchecker(unit):
+    unit = unit.lower()
+    units = ["mm", "cm", "m", "km", "in", "ft", "yd", "mi", "nmi", "au", "ly", "pc", "fathoms", "rods", "chains"]
+    for x in units:
+        if unit == x:
+            return unit
+        else:
+            pass
+
+    print("\nPlease enter a valid unit of measurement.")
+    unit = unitchecker(input("\nRetry to enter the unit of measurement: "))
+    return unit
